3a2ba93e1d005080ebc42781b00f470d749b76f1
JeongSoo Lee <volcano0909@gmail.com>
2020-12-29 20:46:09 +0900

    add uv, fine-dust, ultra-fine-dust

--- scrape_weather.el
+++ scrape_weather.el
@@ -3,22 +3,34 @@
 (require 'cl-lib)
 
 
-;;; Parse the target website's HTML.
+;;; Parse the target website's HTML. ================
+;;; =================================================
 
 
-(defun parse-html (url)
+(defun parse-naver-weather-html ()
   "parse the HTML document retrieved by making a request to a given url."
-  (set-buffer (url-retrieve-synchronously url))
-  (libxml-parse-html-region (point-min) (point-max)))
-
-
-(defvar *naver-weather-html* (caddr (parse-html "https://weather.naver.com")))
+  (set-buffer (url-retrieve-synchronously "https://weather.naver.com/"))
+  (let ((naver (libxml-parse-html-region (point-min) (point-max))))
+    (caddr naver)))
 
 
 (defun find-by-tag (data tag)
   (let ((res))
     (subst nil nil data :test (lambda (a b)
-                                (when (and (listp b) (eql (car b) tag))
+                                (when (and (listp b)
+                                           (eql (car b) tag))
+                                  (push b res))
+                                nil))
+    res))
+
+
+(defun find-by-tag-and-attribute (data tag attr)
+  "find the tag containing"
+  (let ((res))
+    (subst nil nil data :test (lambda (a b)
+                                (when (and (listp b)
+                                           (eql (car b) tag)
+                                           (-contains? (get-attribute-list b) attr))
                                   (push b res))
                                 nil))
     res))
@@ -28,8 +40,8 @@
   (and (listp list) (not (listp (cdr list))))) 
 
 
-(defun get-attribute-list (tag)
-  (let ((data (cadr tag)))
+(defun get-attribute-list (parsed-html-fragment)
+  (let ((data (cadr parsed-html-fragment)))
     (when (-all? #'dotted-pairp data)
       data)))
 
@@ -74,7 +86,45 @@
     acc))
 
 
-;;; Organize the information on a new org-buffer.
+(defun info-to-string-inner (acc list)
+  (if (null list)
+      acc
+    (destructuring-bind (tmpr ymdt weather) (car list)
+      (info-to-string-inner (concat acc ymdt " " tmpr "도 " weather "\n") (cdr list)))))
+
+
+(defun info-to-string ()
+  (info-to-string-inner "" (seq-take (get-hourly-weather (parse-naver-weather-html)) 13)))
+
+
+(defun display-hourly-weather ()
+  (interactive)
+  (message (info-to-string)))
+
+
+(defun has-attribute-value (attr-list value)
+  (reduce (lambda (acc attr) (or acc (equal (cdr attr) value)))
+          attr-list
+          :initial-value nil))
+
+
+(defun extract-sunset-uv-fd-ufd (ul)
+  (assert (has-attribute-value (get-attribute-list ul) "today_chart_list"))  ; attribute 중에 "today_chart_list"가 있는지 확인
+  (let* ((raw-data (find-by-tag ul 'strong))
+         (data (mapcar #'caddr raw-data))
+         (raw-values (find-by-tag ul 'em))
+         (values (mapcar #'caddr raw-values)))
+    (-zip data values)))
+
+
+(defun get-sunset-uv-fd-ufd ()
+  (interactive)
+  (let ((ul (find-by-tag-and-attribute (parse-naver-weather-html) 'ul '(class . "today_chart_list"))))
+    (extract-sunset-uv-fd-ufd (car ul))))
+
+
+;;; Organize the information on a new org-buffer. ====
+;;; ==================================================
 
 
 (defun create-buffer ()
@@ -84,8 +134,15 @@
     new-buffer))
 
 
+(defun insert-title (buffer)
+  "Precondition: the buffer's major mode should be org-mode"
+  (insert "* Naver Weather"))
+
+
 (defun create-org-table (buffer)
+  ;; TODO
   )
+
 
 ;; TODO draw a nice table containing information retrieved by get-hourly-weather.
 ;; TODO find a means to calculate the table width
